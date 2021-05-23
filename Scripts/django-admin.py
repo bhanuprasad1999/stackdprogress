972571cb4875b977dba652e5c2110f64ea2da98b
bhanuprasad1999 <bhanuprasad1999@github.com>
2021-05-23 08:14:28 +0530

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Courses\Learning-Courses\Programming\Django Framework\Projects\stackdprogress\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
